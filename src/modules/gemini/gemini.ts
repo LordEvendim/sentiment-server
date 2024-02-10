fc35c1bfdc9a21c7923bb7314b689d0f916d2df6
LordEvendim <bartezy123@gmail.com>
2024-02-10 09:48:31 +0100

    chore: gemini integration

---
+++ src/modules/gemini/gemini.ts
@@ -0,0 +1,33 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+class Gemini {
+  genAI: GoogleGenerativeAI;
+
+  constructor() {
+    this.genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY ?? "");
+  }
+
+  testResponse = async (input: string) => {
+    const model = this.genAI.getGenerativeModel({ model: "gemini-pro" });
+
+    const prompt = "Write a story about a magic backpack.";
+
+    const result = await model.generateContent(prompt);
+    const response = result.response;
+    const text = response.text();
+    console.log(text);
+  };
+
+  getTextResponse = async (input: string) => {
+    const model = this.genAI.getGenerativeModel({ model: "gemini-pro" });
+
+    const result = await model.generateContent(input);
+
+    const response = result.response;
+    const text = response.text();
+
+    return text;
+  };
+}
+
+export const gemini = new Gemini();
