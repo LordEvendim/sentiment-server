e9016a8d01b5d11af53a5d05450c4c5d52530f20
LordEvendim <24799163+LordEvendim@users.noreply.github.com>
2024-08-19 00:03:56 +0200

    feat: user roles

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,6 +1,8 @@
 import { relations } from "drizzle-orm";
 import { int, mysqlTable, text, varchar } from "drizzle-orm/mysql-core";
 
+import { USER_ROLES } from "#modules/authorization/roles";
+
 import { googleIntegrations } from "./googleIntegrations";
 import { metaIntegrations } from "./metaIntegrations";
 import { reports } from "./reports";
@@ -12,6 +14,7 @@ export const users = mysqlTable("users", {
   password: text("password").notNull(),
   email: varchar("email", { length: 256 }).notNull(),
   credits: int("credits").default(0).notNull(),
+  role: varchar("role", { length: 32 }).default(USER_ROLES.USER),
 });
 
 export const usersRelations = relations(users, ({ one, many }) => ({
