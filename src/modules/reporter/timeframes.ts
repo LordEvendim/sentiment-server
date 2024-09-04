61569e14d51a4cafc72ad570e5632daa344d5161
LordEvendim <24799163+LordEvendim@users.noreply.github.com>
2024-09-04 21:47:54 +0200

    feat: additional timeframes

--- src/modules/reporter/timeframes.ts
+++ src/modules/reporter/timeframes.ts
@@ -4,6 +4,7 @@ export type DashboardTimeframe =
   | "last-7-days"
   | "last-14-days"
   | "last-30-days"
+  | "last-60-days"
   | "last-90-days"
   | "last-6-months"
   | "last-year";
@@ -12,6 +13,7 @@ export const DashboardTimeframes: Record<string, DashboardTimeframe> = {
   LAST_7_DAYS: "last-7-days",
   LAST_14_DAYS: "last-14-days",
   LAST_30_DAYS: "last-30-days",
+  LAST_30_DAYS: "last-60-days",
   LAST_90_DAYS: "last-90-days",
   LAST_6_MONTHS: "last-6-months",
   LAST_YEAR: "last-year",
@@ -22,6 +24,7 @@ const timeframeStartFunctions: Record<DashboardTimeframe, (end: Date) => Date> =
     "last-7-days": (end) => subDays(end, 7),
     "last-14-days": (end) => subDays(end, 14),
     "last-30-days": (end) => subDays(end, 30),
+    "last-60-days": (end) => subDays(end, 60),
     "last-90-days": (end) => subDays(end, 90),
     "last-6-months": (end) => subMonths(end, 6),
     "last-year": (end) => subYears(end, 1),
